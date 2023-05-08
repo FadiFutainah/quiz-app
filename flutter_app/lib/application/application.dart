a2a589f1736440de82dda8990f963efed2d47b7f
Fadi Futainah <fadiftenaft@gmail.com>
2023-05-08 05:04:50 +0300

    add the basic packages

---
+++ flutter_app/lib/application/application.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
